b4a8cc46a0de9cdcc376a95ce47fc827f8b70bdc
Sai Shanmukha <nss10@outlook.com>
2025-10-09 16:11:02 -0500

    fix: typo

--- plugin-gen3/auth_impl.go
+++ plugin-gen3/auth_impl.go
@@ -175,7 +175,7 @@ func (a Authorize) PluginAction(params map[string]string, headers map[string]*pr
 	if err != nil {
 		return errorResponse(http.StatusInternalServerError, fmt.Sprintf("could not parse '%s' response body: %w", url, err))
 	}
-	Shared.Logger.Info("Printing task tags", "Tags", task.Tags)
+	shared.Logger.Info("Printing task tags", "Tags", task.Tags)
 	if workerRoleArn, ok := task.Tags["funnel_worker_role_arn"]; ok {
 		shared.Logger.Info("Using worker role arn from task tags", "funnel_worker_role_arn", workerRoleArn)
 	}
